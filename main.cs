655d586663c699deeddc5c29c56ddefa2c09a263
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2022-10-05 14:09:20 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int a;  //Declearaction
+    int b;  //Declearaction
+    int answer;
+    a = 10;
+    b = 11;
+    answer = a % b;
+      
+    Console.WriteLine (answer);
+  }
+}
