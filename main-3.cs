22970c38ead0a1cf2d8085f0feb729ea7551bde1
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2022-10-07 14:01:43 -0400

    Add files via upload

---
+++ main-3.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  
+    int  x= 10, y= 20;
+    if(x>y){
+      //Statement //true
+      Console.WriteLine ("x is greater than y");
+    }
+  
+    Console.WriteLine ("Hello World");
+ 
+       
+       //Console.WriteLine (x>y);
+   // Console.WriteLine (x<y);
+    //Console.WriteLine (x=y); // Assignment operator
+//Console.WriteLine (x==y); //20 ==20
+  }
+}
