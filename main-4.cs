d6cf535e4ea4bbfaf45f13cc0d08c7cf7700cbda
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2022-10-12 13:03:50 -0400

    Add files via upload

---
+++ main-4.cs
@@ -0,0 +1,20 @@
+using System;
+
+ class Program {
+   public static void Main (string[] args) {
+
+     String name;
+     name ="Timothy";
+     //const double pi=3.14;
+
+     //radius=5; //initialization
+     //area= pi*radius*radius;
+
+    // area1=pi*radius1*radius1;
+
+     //Console.WriteLine ("Area is           "+area);// + is used to append the value 
+    // Console.WriteLine("Area is"+area1);
+     Console.Writeline(Timothy);
+     
+   }
+ }
