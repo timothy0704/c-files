3b23dc31265d4b2335f77878cd95dbde94fde393
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2022-11-01 13:29:08 -0400

    Add files via upload

---
+++ main-13.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+int i = 1;
+    do
+  {
+    
+    Console.WriteLine ("Hello World");
+      i++;
+    }    while(i<5);
+  }
+}
